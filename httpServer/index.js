b9b585d04c1c736f1bcc5cf3c4dc918400779e20
prasanv <bala.prasanv@gmail.com>
2023-11-20 20:20:27 -0800

    httpServer with json response

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,10 +1,24 @@
 const http = require("node:http");
 
+
+// Response sent as text/plain
+// const server = http.createServer((req, res) => {
+//     console.log({req, res})
+//     res.writeHead(200, {"Content-Type":"text/plain"});
+//     res.end("Hello from Server");
+// })
+
+// Response as json 
 const server = http.createServer((req, res) => {
-    console.log({req, res})
-    res.writeHead(200, {"Content-Type":"text/plain"});
-    res.end("Hello from Server");
+    const userName = {
+        firstName: "Prasan",
+        lastName: "Bala"
+    }
+    res.writeHead(200, {"Content-Type":"application/json"});
+    // convert the jsn object to string before sending or you will see an error 
+    res.end(JSON.stringify(userName));
 })
+
 
 server.listen(3030, () => {
     console.log("server running on port 3030")
