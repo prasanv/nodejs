5ed8f9cfcd0f73b5d6b2bfa68ca862d726ec95d7
prasanv <bala.prasanv@gmail.com>
2023-11-20 20:11:21 -0800

    added httpServer

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,3 +1,11 @@
 const http = require("node:http");
 
-console.log(http);
+const server = http.createServer((req, res) => {
+    console.log({req, res})
+    res.writeHead(200, {"Content-Type":"text/plain"});
+    res.end("Hello from Server");
+})
+
+server.listen(3030, () => {
+    console.log("server running on port 3030")
+})
