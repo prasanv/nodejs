d2289f3f7c2940dfd583a3c85b003a4257a2be27
prasanv <bala.prasanv@gmail.com>
2023-11-25 13:03:41 -0800

    event module example

---
+++ eventsModule/index.js
@@ -0,0 +1,21 @@
+const eventEmitter = require("node:events");
+
+const emitter = new eventEmitter();
+
+// This is called event driven programming
+emitter.on("order-pizza", (size, topping) => {
+    if(size && topping){
+        console.log(`Baking ${size.toLowerCase()} ${topping.toLowerCase()} pizza!`)
+    } else {
+        console.log(`Baking pizza!`)
+    }
+});
+
+// you can add multiple listeners to the sam event
+emitter.on("order-pizza", (...args) => {
+    const size = args[0];
+    if(size.toString().toLowerCase() === "large")
+    console.log("Please don't forget to pick up your free drink");
+})
+
+emitter.emit("order-pizza", "LARGE", "mushroom");
