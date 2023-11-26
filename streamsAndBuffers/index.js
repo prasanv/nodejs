5c0138089a916f24caee084d02283e2025564877
prasanv <bala.prasanv@gmail.com>
2023-11-26 11:54:43 -0800

    streams and buffers

---
+++ streamsAndBuffers/index.js
@@ -0,0 +1,22 @@
+// console.log(Buffer);
+
+console.log(Buffer);
+
+// DEfaults to UTF-8
+// const buffer = new Buffer.from("PrasanV", "UTF-8")
+
+const buffer = new Buffer.from("Prasan Venkat")
+
+// Based on the above from value, if the characters are less then it will just overwrite required char
+buffer.write("Tamil Nadu");
+
+// Based on the above from value, buffer has limited space cannot fit every character
+// buffer.write("Salem, TamilNadu");
+
+console.log(buffer.toString());
+
+// Returns raw binary data, HexaDecimal representation
+console.log(buffer);
+
+// Returns utf-8 character encoded value
+console.log(buffer.toJSON());
