4a5f1dedecf06ee2f73909c1cadabb48dbbf8e24
prasanv <bala.prasanv@gmail.com>
2023-12-04 17:43:58 -0800

    basic email authentication using hmac

---
+++ 12-crypto/hmac.js
@@ -0,0 +1,45 @@
+const {scrypt, scryptSync, randomBytes, randomUUID, timingSafeEqual} = require("node:crypto");
+
+// Hash-based message authentication code (or HMAC) is a cryptographic authentication technique 
+// that uses a hash function and a secret key.
+
+// scryptSync(password, salt, keylen[, options])
+// crypto.scrypt(password, salt, keylen[, options], callback)
+
+// crypto.randomBytes(size[, callback])
+// crypto.randomUUID([options])
+
+
+const hashUserPassword = (email, password) => {
+    const salt = randomBytes(64).toString("hex");
+    scrypt(password, salt, 64, (err, hashedKey) => {
+        if (err) throw err;
+
+        // steps to store it to DB
+        const uuid = randomUUID();
+        const user = {uuid, email, password: `${salt}:${hashedKey.toString('hex')}`}
+        console.log(user);
+    })
+}
+
+hashUserPassword("prasanv@gmail.com","Asdf!2345");
+
+const login = (email, password) => {
+    const [salt, key] = password.split(':');
+    const hashedBuffer = scryptSync(password, salt, 64)
+    
+    const keyBuffer = Buffer.from(hashedBuffer, 'hex');
+    const match = timingSafeEqual(hashedBuffer, keyBuffer);
+
+    console.log({keyBuffer, match})
+
+    if(match){
+        console.log('Login success');
+    } else {
+        console.log('Login fail');
+    }
+    
+
+}
+
+login("prasanv@gmail.com","Asdf!2345");
