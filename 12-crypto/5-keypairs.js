f8602e3aa470195cf7455bd2ad3e08831c4d0262
prasanv <bala.prasanv@gmail.com>
2023-12-05 22:09:05 -0800

    generate rsa key pairs

---
+++ 12-crypto/5-keypairs.js
@@ -0,0 +1,29 @@
+const { generateKeyPairSync } = require('node:crypto');
+
+// crypto.generateKeyPairSync(type, options)
+// crypto.generateKeyPair(type, options, callback)
+
+// Generates a new asymmetric key pair of the given type. RSA, RSA-PSS, DSA, EC, Ed25519, Ed448, X25519, X448, and DH are currently supported.
+// When encoding public keys, it is recommended to use 'spki'. When encoding private keys, it is recommended to use 'pkcs8' with a strong passphrase, and to keep the passphrase confidential.
+
+
+const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+    modulusLength : 1024, // can be 
+    publicKeyEncoding: {
+        type: 'spki', // recommended
+        format: 'pem' 
+    },
+    privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+        cipher: 'aes-256-cbc',
+        passphrase: 'secret-phrase'
+    },
+})
+
+console.log(privateKey, publicKey);
+
+module.exports = {
+    privateKey, 
+    publicKey   
+}
